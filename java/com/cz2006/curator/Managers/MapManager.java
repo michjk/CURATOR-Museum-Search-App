af1539fe91e2796efd3bc0274b42334964ba2e89
Prasanth Karthikeyan <prasanth.karthikeyan001@gmail.com>
2016-03-18 14:27:56 +0800

    added some managers... and some TODOs

---
+++ java/com/cz2006/curator/Managers/MapManager.java
@@ -0,0 +1,8 @@
+package com.cz2006.curator.Managers;
+
+/**
+ * Created by Prasanth on 18/03/2016.
+ */
+public class MapManager {
+
+}
---
+++ java/com/cz2006/curator/Managers/SearchManager.java
@@ -0,0 +1,60 @@
+package com.cz2006.curator.Managers;
+
+import java.util.*;
+
+/**
+ * Created by Prasanth on 18/03/2016.
+ */
+public class SearchManager {
+
+    //attribute(s)
+    private ArrayList<Museum> result;
+
+    //constructor
+    public SearchManager(ArrayList<Museum> result) {
+        this.result = result;
+    }
+
+    //getters & setters
+    public ArrayList<Museum> getResult() {
+        return result;
+    }
+
+    public void setResult(ArrayList<Museum> result) {
+        this.result = result;
+    }
+
+    //methods
+    //TODO make this work
+    public ArrayList<Museum> search(String request) {
+        return result;
+    }
+
+    //TODO make this work
+    public ArrayList<Museum> byProximity() {
+        return result;
+    }
+
+    //TODO test this
+    public Boolean compareByProximity (Museum A, Museum B){
+        double distFromA = Math.sqrt((A.latitude - user.latitude) * (A.latitude-user.latitude)
+                + (A.longitude - user.longitude) * (A.longitude-user.longitude));
+        double distFromB = Math.sqrt((B.latitude - user.latitude) * (B.latitude-user.latitude)
+                + (B.longitude - user.longitude) * (B.longitude-user.longitude));
+        if (distFromA > distFromB)
+            return true;
+        else return false;
+    }
+
+    //TODO make this work
+    public ArrayList<Museum> byRating () {
+        return result;
+    }
+
+    //TODO test this
+    public Boolean compareByRating (Museum A, Museum B) {
+        if (A.rating > B.rating)
+            return true;
+        else return false;
+    }
+}
