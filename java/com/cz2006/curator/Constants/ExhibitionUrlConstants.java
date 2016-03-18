385b0192e931d46635b24b134e54cc045ce55846
michjk <michaelj.kosasih@gmail.com>
2016-03-18 15:22:47 +0800

    exhibition url constant for crawling from specific museum

---
+++ java/com/cz2006/curator/Constants/ExhibitionUrlConstants.java
@@ -0,0 +1,10 @@
+package com.cz2006.curator.Constants;
+
+import java.util.HashMap;
+
+/**
+ * Created by Acceleration on 18/03/2016.
+ */
+public class ExhibitionUrlConstants {
+    HashMap<String>
+}
