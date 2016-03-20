768bf85dc8b1bbae69fe0d67bf8ef1a2ef42de87
michjk <michaelj.kosasih@gmail.com>
2016-03-20 13:33:57 +0800

    delete unnecessary folder

--- java/com/cz2006/curator/Constants/ExhibitionUrlConstants.java
+++ java/com/cz2006/curator/Constants/ExhibitionUrlConstants.java
@@ -6,5 +6,12 @@ import java.util.HashMap;
  * Created by Acceleration on 18/03/2016.
  */
 public class ExhibitionUrlConstants {
-    HashMap<String>
+    private static HashMap<String, String> exhibitionUrl = new HashMap<String, String>() {
+        {
+            put("Art Science Museum","http://www.marinabaysands.com/museum/exhibitions-and-events.html");
+            put("National Museum of Singapore", "http://nationalmuseum.sg/exhibitions/exhibition-list");
+            put("Singapore Art Museum", "https://www.singaporeartmuseum.sg/exhibitions/current.html");
+            put("National Gallery Singapore", "https://www.nationalgallery.sg/see-do/exhibitions");
+        }
+    };
 }
