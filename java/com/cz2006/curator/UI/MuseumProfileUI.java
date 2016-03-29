ebb3df7aad7076896667a2fd6170a88928ec81c4
Vicsonlee <lee_vicson@hotmail.com>
2016-03-29 21:42:47 +0800

    added museumUI

---
+++ java/com/cz2006/curator/UI/MuseumProfileUI.java
@@ -0,0 +1,20 @@
+package com.cz2006.curator.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.cz2006.curator.R;
+
+public class MuseumProfileUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_museum_profile_ui);
+    }
+
+}
