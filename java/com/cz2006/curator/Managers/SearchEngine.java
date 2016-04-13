5dc6d2394aeedf2ba6eca1f757d704dc6b8e9d48
ericleoo <ericl619@gmail.com>
2016-04-13 15:46:08 +0800

    Search UI sorted by proximity by defult

--- java/com/cz2006/curator/Managers/SearchEngine.java
+++ java/com/cz2006/curator/Managers/SearchEngine.java
@@ -20,8 +20,8 @@ public class SearchEngine{
     private User user;
 
     //constructor
-    public SearchEngine(ArrayList<Museum> arr) {
-        User u = new User(0,0);
+    public SearchEngine(ArrayList<Museum> arr, User u) {
+        user = u;
         result = arr;
     }
 
@@ -47,29 +47,39 @@ public class SearchEngine{
     }
 
     public ArrayList<Museum> byProximity() {
-        //Collections.sort(result, new CmpByProximity());
+        Collections.sort(result,new CmpByProximity());
         return result;
     }
 
     public ArrayList<Museum> byRating() {
-        search();
+        //search();
         //Collections.sort(result, new CmpByRating());
         return result;
     }
 
     public class CmpByProximity implements Comparator<Museum> {
+
+        public double dist(double lat1, double lon1, double lat2, double lon2){
+            double R = 6371000;
+            double p1 = Math.toRadians(lat1);
+            double p2 = Math.toRadians(lat2);
+            double dp = Math.toRadians(lat2-lat1);
+            double dl = Math.toRadians(lon2-lon1);
+
+            double a = Math.sin(dp/2.0) * Math.sin(dp/2.0)
+                        + Math.cos(p1/2.0) * Math.cos(p2/2.0) * Math.sin(dl/2.0) * Math.sin(dl/2.0);
+
+            double c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            return R * c;
+        }
+
         @Override
         public int compare(Museum a, Museum b) {
-            double distFromA = Math.sqrt(
-                    (a.getLatitude() - user.getLatitude()) * (a.getLatitude() - user.getLatitude()) +
-                            (a.getLongitude() - user.getLongitude()) * (a.getLongitude() - user.getLongitude())
-            );
-            double distFromB = Math.sqrt(
-                    (b.getLatitude() - user.getLatitude()) * (b.getLatitude() - user.getLatitude()) +
-                            (b.getLongitude() - user.getLongitude()) * (b.getLongitude() - user.getLongitude())
-            );
+            double d1 = dist(a.getLatitude(),a.getLongitude(),user.getLatitude(),user.getLongitude());
+            double d2 = dist(b.getLatitude(),b.getLongitude(),user.getLatitude(),user.getLongitude());
 
-            return ((distFromA < distFromB)?(-1):(1));
+            return ((d1 < d2)?(-1):(1));
         }
     }
 
