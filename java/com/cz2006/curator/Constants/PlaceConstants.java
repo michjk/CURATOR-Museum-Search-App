5fe0b2b68e8a4e7bdba5db1e853f97bf9708a070
Prasanth Karthikeyan <prasanth.karthikeyan001@gmail.com>
2016-04-14 23:09:06 +0800

    added fourth API key... Don't tell Google

--- java/com/cz2006/curator/Constants/PlaceConstants.java
+++ java/com/cz2006/curator/Constants/PlaceConstants.java
@@ -5,12 +5,11 @@ package com.cz2006.curator.Constants;
  */
 public class PlaceConstants {
 
-    /*  <<- Add/remove one '/' here to toggle between key 1 and key 2
-    private static final String API_KEY = "AIzaSyDuxQ0E7rKTxqVWDvr2YQegWQWJ1wJnuT0";
-    /*/
+    //select only one of the following keys at a time
+    private static final String API_KEY = "AIzaSyAMA_EoylT2Dz_grch1O9Wd8bziEpKzAg4";
+    //private static final String API_KEY = "AIzaSyDuxQ0E7rKTxqVWDvr2YQegWQWJ1wJnuT0";
     //private static final String API_KEY = "AIzaSyAiaOUB4sQnnkyNZeo63hGHjFrYX8UEB6E";
-    //*/
-    private static final String API_KEY = "AIzaSyBu-K8O-hJF_iByWggp7y6b36KmBnb7xI0";
+    //private static final String API_KEY = "AIzaSyBu-K8O-hJF_iByWggp7y6b36KmBnb7xI0";
     public static String URL = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=museums+in+Singapore&key=" + API_KEY;
     public static final String KEY_ITEM = "result"; // parent node
     public static final String KEY_ID = "place_id";
